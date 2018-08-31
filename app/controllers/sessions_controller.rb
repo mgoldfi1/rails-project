8d7dcda562e622361f45007d78d403c4b8eb76cb
mgoldfi1 <mgoldfi1@gmail.com>
2018-08-31 12:03:52 -0700

    logout function and sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,19 +1,27 @@
 class SessionsController < ApplicationController
 
   def new
-    @user = User.new 
   end
 
   def create
-    @user = User.find_or_create_by(uid: auth['uid']) do |u|
-      u.name = auth['info']['name']
-      u.email = auth['info']['email']
-      u.image = auth['info']['image']
+    @user = User.find_or_create_by(email: auth['email'])
+    session[:user_id] = @user.id
+    redirect_to user_path(@user)
+  end
+
+  def login
+    @user = User.find_by(name: params[:user][:username])
+    if @user && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
     end
 
-    session[:user_id] = @user.id
-
-    render 'welcome/home'
+  def destroy
+    session.delete :user_id
+    redirect_to login_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,12 @@ Rails.application.routes.draw do
   root 'users#home'
 
   get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
+  post '/login', to: 'sessions#login'
 
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
+
+  get '/logout', to: 'sessions#destroy'
 
   resources :users
 end
