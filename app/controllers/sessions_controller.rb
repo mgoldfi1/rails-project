b93f3ca723c6c7427412453696ce6ad656b90cc4
mgoldfi1 <mgoldfi1@gmail.com>
2018-09-04 14:59:07 -0700

    login flash message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -23,6 +23,7 @@ class SessionsController < ApplicationController
       session[:user_id] = @user.id
       redirect_to user_showings_path(@user)
     else
+      flash[:notice] = "Invalid username or password"
       render :new
     end
     end
